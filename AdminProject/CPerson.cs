a074ec9109d1d0ef6db30fdfb0d298db1361332e
Zora <zoraartz@outlook.com>
2022-08-30 16:19:25 -0500

    A new class CPerson and CStudent has been created.

---
+++ AdminProject/CPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminProject
+{
+    public abstract class CPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public int Age { get; set; }
+
+        public abstract void SetInfo();
+        public abstract void GetInfo();
+
+    }
+}
---
+++ AdminProject/CStudetn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminProject
+{
+    public class CStudetn : CPerson
+    {
+        public override void GetInfo()
+        {
+            
+        }
+
+        public override void SetInfo()
+        {
+            Console.WriteLine("<-- Personal Information -->");
+            Console.Write("Your Name: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Your Last Name: ");
+            LastName = Console.ReadLine();
+            Console.Write("Your Age: ");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Id: ");
+            Id = (int)Convert.ToInt64(Console.ReadLine());
+
+            SetInfoStudent();
+        }
+
+        public void SetInfoStudent()
+        {
+            Console.WriteLine("<-- Complete student information -->");
+            Console.Write("Grade currently in school: ");
+            degree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Modality of study: ");
+            modality = Console.ReadLine();
+        }
+
+        private string modality;
+        private int degree;
+    }
+}
