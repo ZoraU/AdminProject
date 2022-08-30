1a580a7aaba787d5334f1aa4b15bbf8fb6b87fb8
Ryzen <Ryzen@DESKTOP-4LSVKUG>
2022-08-29 23:31:24 -0500

    Agregar archivos de proyecto.

---
+++ AdminProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
